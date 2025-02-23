eceb6be4cb5981117a37324946f52f2c0ddb6617
LAVG00 <lavg8759@gmail.com>
2025-02-22 21:27:18 -0600

    implemented POST operation

--- BlazorTrainingBE/Program.cs
+++ BlazorTrainingBE/Program.cs
@@ -3,6 +3,9 @@ using BlazorTrainingBE.Dtos;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+
+const string GetGameEndPintName = "GetGame";
+
 List<GameDto> games = [
     new(1, "Dark Souls", "RPG", 549.00M, new DateOnly(2011, 9, 22)),
     new(2, "Pokemon Platinum", "RPG", 800.00M, new DateOnly(2008, 9, 13)),
@@ -13,9 +16,15 @@ List<GameDto> games = [
 app.MapGet("games", () => games);
 
 //GET /game/{id}
-app.MapGet("games/{id}", (int id) => games.Find(game => game.Id == id));
+app.MapGet("games/{id}", (int id) => games.Find(game => game.Id == id)).WithName(GetGameEndPintName);
 
-//Post /game
+//POST /games
+app.MapPost("games", (CreateGameDto newGame) =>
+{
+    GameDto game = new(games.Count + 1, newGame.Name, newGame.Genre, newGame.Price, newGame.ReleaseDate);
+    games.Add(game);
+    return Results.CreatedAtRoute(GetGameEndPintName, new { id = game.Id }, game);
+});
 
 
 app.Run();
